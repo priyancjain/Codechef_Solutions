6aa8f89e87fcd416b512b755d70f0bc4e53cef9f
priya-jan <priyanshijain1628@gmail.com>
2023-03-04 16:56:05 +0530

    Second

---
+++ LP1TO201/CHFRICH.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,a,b,c;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>a>>b>>c;
+	    int d=(b-a)/c;
+	    cout<<d<<endl;
+	}
+	return 0;
+}
+
